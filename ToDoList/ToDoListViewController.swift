d6efd5cc60d37daa231c8b43dc9a9fad27912df2
InnaKokorina <93093046+InnaKokorina@users.noreply.github.com>
2022-03-04 17:24:39 +0400

    Initial Commit

---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Inna Kokorina on 04.03.2022.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
