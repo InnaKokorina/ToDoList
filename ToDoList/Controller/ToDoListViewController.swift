abb4531d179c92dd7c13480ae2ee8931428c5f0c
InnaKokorina <93093046+InnaKokorina@users.noreply.github.com>
2022-03-09 11:28:36 +0400

    change from NSCoder to CoreData: add "save context"

--- ToDoList/Controller/ToDoListViewController.swift
+++ ToDoList/Controller/ToDoListViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ToDoListViewController: UITableViewController {
     
-var itemArray = [Item]()
+    var itemArray = [Item]()
     
-let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,22 +24,22 @@ let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDo
         
         print(dataFilePath)
         
-    
-        loadItems()
+        
+      //  loadItems()
         
     }
-//MARK - TableView Datasource Method
+    //MARK - TableView Datasource Method
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let item = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
-
+        
         return cell
     }
     
@@ -56,18 +60,19 @@ let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDo
         
         let alert = UIAlertController(title: "Add new ToDo item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add action", style: .default) { action in
-        
-            let newItem = Item()
+           
+            let newItem = Item(context: self.context)
             newItem.title = textFied.text!
+            newItem.done = false
             
             self.itemArray.append(newItem)
             
             self.saveItems()
-           
+            
         }
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new item"
-          textFied = alertTextField
+            textFied = alertTextField
             
         }
         alert.addAction(action)
@@ -76,25 +81,24 @@ let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDo
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
+       
         do {
-            let data =  try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+          try context.save()
         } catch {
-            print("Error encoding item array,\(error)")
+            print("Error saving context \(error)")
         }
-   
+        
         self.tableView.reloadData()
     }
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error dencoding item array,\(error)")
-            }
-        }
-    }
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//            do {
+//                itemArray = try decoder.decode([Item].self, from: data)
+//            } catch {
+//                print("Error dencoding item array,\(error)")
+//            }
+//        }
+//    }
 }
 
